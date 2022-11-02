c25084e891c0ff69121af85bda0963a9e48a5a94
bgdnvsky <raevskijb@gmail.com>
2022-11-02 18:21:45 +0100

    Just an empty main file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 mod tokens;
+use std::fs;
 
 fn main() {
-  let content = String::from("macro print do");
+  /*
+  let content = fs::read_to_string("test.nn").expect("Should have been able to read the file");
   let tokenizer = tokens::Tokenizer::new(content);
 
   for token in tokenizer.tokenize() {
     println!("{}", token);
   }
+  */
 }
