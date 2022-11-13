5d3172eb0505a39bc8414969f3c931d2e9e9af5a
bgdnvsky <raevskijb@gmail.com>
2022-11-13 13:55:47 +0100

    Nothing interesting, plain empty main file for testing new stuff

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
-mod tokens;
-use std::fs;
+mod gen;
+use gen::*;
 
 fn main() {
-  /*
-  let content = fs::read_to_string("test.nn").expect("Should have been able to read the file");
-  let tokenizer = tokens::Tokenizer::new(content);
-
-  for token in tokenizer.tokenize() {
-    println!("{}", token);
-  }
-  */
 }
